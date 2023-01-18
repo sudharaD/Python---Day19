613a6e48bc0c22c32cf850e6ed3eef5884a22747
sudharaD <sudharad7@gmail.com>
2023-01-18 23:39:43 +0530

    sketch app finished

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 # Higher order functions
 import turtle as t
 from higher_order_functions import HigherOrderFunctions
+from sketch_app import Sketch
 
 turtle = t.Turtle()
 screen = t.Screen()
 screen.listen()
 
-higher_order_function = HigherOrderFunctions()
+# higher_order_function = HigherOrderFunctions()
+# # screen.onkey(fun=higher_order_function.draw, key="space")
+# # print(turtle.heading())
 
-screen.onkey(fun=higher_order_function.draw, key="space")
+sketch_app = Sketch()
+screen.onkeypress(fun=sketch_app.forward, key="w")
+screen.onkeyrelease(fun=sketch_app.stop, key="w")
+screen.onkeypress(fun=sketch_app.backward, key="s")
+screen.onkeyrelease(fun=sketch_app.stop, key="s")
+screen.onkeypress(fun=sketch_app.right, key="d")
+screen.onkeyrelease(fun=sketch_app.stop, key="d")
+screen.onkeypress(fun=sketch_app.left, key="a")
+screen.onkeyrelease(fun=sketch_app.stop, key="a")
+screen.onkey(fun=sketch_app.clear, key="c")
+
+# screen.onkey(fun=sketch_app.forward, key="w")
 
 screen.exitonclick()
 
--- sketch_app.py
+++ sketch_app.py
@@ -1,3 +1,23 @@
+import turtle
 from turtle import Turtle
 
 class Sketch:
+    turtle = Turtle()
+
+    def forward(self):
+        turtle.forward(5)
+
+    def backward(self):
+        turtle.backward(5)
+
+    def stop(self):
+        turtle.forward(5)
+
+    def left(self):
+        turtle.left(5)
+
+    def right(self):
+        turtle.right(5)
+
+    def clear(self):
+        turtle.reset()
